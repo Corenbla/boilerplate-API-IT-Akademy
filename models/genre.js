46395ed23b1a854c6e6463833589b4ba6b9d91ee
corenbla <corenbla@gmail.com>
2021-04-14 20:53:41 +0200

    cs

--- models/genre.js
+++ models/genre.js
@@ -1,23 +1,25 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Genre extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      this.hasMany(models.Movie)
+    class Genre extends Model
+    {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            this.hasMany(models.Movie)
+        }
     }
-  };
-  Genre.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Genre',
-  });
-  return Genre;
+
+    Genre.init({
+        name: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Genre',
+    });
+    return Genre;
 };
